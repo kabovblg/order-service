b594f187281b54d9f49d4ba4babe5cd3aad52178
Blagovest Kabov <kabov.blg@gmail.com>
2024-12-07 16:43:40 +0200

    unit tests

---
+++ src/test/java/com/blagovestkabov/order_service/controller/OrderControllerTest.java
@@ -0,0 +1,176 @@
+package com.blagovestkabov.order_service.controller;
+
+import com.blagovestkabov.order_service.entity.Order;
+import com.blagovestkabov.order_service.model.OrderRequest;
+import com.blagovestkabov.order_service.model.OrderResponse;
+import com.blagovestkabov.order_service.repository.OrderRepository;
+import com.blagovestkabov.order_service.service.OrderService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static java.nio.charset.Charset.defaultCharset;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.util.StreamUtils.copyToString;
+
+@Disabled
+@SpringBootTest({"server.port=0"})
+@AutoConfigureMockMvc
+public class OrderControllerTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @RegisterExtension
+    static WireMockExtension wireMockServer
+            = WireMockExtension.newInstance()
+            .options(WireMockConfiguration
+                    .wireMockConfig()
+                    .port(8080))
+            .build();
+
+    private ObjectMapper objectMapper
+            = new ObjectMapper()
+            .findAndRegisterModules()
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    @BeforeEach
+    void setup() throws IOException {
+        getProductDetailsResponse();
+        doPayment();
+        getPaymentDetails();
+        reduceQuantity();
+    }
+
+    private void reduceQuantity() {
+        wireMockServer.stubFor(put(urlMatching("/product/reduceQuantity/.*"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)));
+    }
+
+    private void getPaymentDetails() throws IOException {
+        wireMockServer.stubFor(get(urlMatching("/payment/.*"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(
+                                copyToString(
+                                        OrderControllerTest.class
+                                                .getClassLoader()
+                                                .getResourceAsStream("mock/GetPayment.json"),
+                                        defaultCharset()
+                                )
+                        )));
+    }
+
+    private void doPayment() {
+        wireMockServer.stubFor(post(urlEqualTo("/payment"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)));
+    }
+
+    private void getProductDetailsResponse() throws IOException {
+        wireMockServer.stubFor(get("/product/1")
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(copyToString(
+                                OrderControllerTest.class
+                                        .getClassLoader()
+                                        .getResourceAsStream("mock/GetProduct.json"),
+                                defaultCharset()
+                        ))));
+    }
+
+    private OrderRequest getMockOrderRequest() {
+        return OrderRequest.builder()
+                .productId(1)
+                .paymentMethod(null) // No need for enums if not using them
+                .quantity(10)
+                .totalAmount(200)
+                .build();
+    }
+
+    @Test
+    public void test_WhenPlaceOrder_DoPayment_Success() throws Exception {
+        OrderRequest orderRequest = getMockOrderRequest();
+        MvcResult mvcResult
+                = mockMvc.perform(MockMvcRequestBuilders.post("/order/placeorder")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(orderRequest))
+                ).andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String orderId = mvcResult.getResponse().getContentAsString();
+
+        Optional<Order> order = orderRepository.findById(Long.valueOf(orderId));
+        assertTrue(order.isPresent());
+
+        Order o = order.get();
+        assertEquals(Long.parseLong(orderId), o.getId());
+        assertEquals("PLACED", o.getOrderStatus());
+        assertEquals(orderRequest.getTotalAmount(), o.getAmount());
+        assertEquals(orderRequest.getQuantity(), o.getQuantity());
+    }
+
+    @Test
+    public void test_WhenGetOrder_Success() throws Exception {
+        MvcResult mvcResult
+                = mockMvc.perform(MockMvcRequestBuilders.get("/order/1")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String actualResponse = mvcResult.getResponse().getContentAsString();
+        Order order = orderRepository.findById(1L).get();
+        String expectedResponse = getOrderResponse(order);
+
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    public void testWhen_GetOrder_Order_Not_Found() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/order/999")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    private String getOrderResponse(Order order) throws IOException {
+        OrderResponse orderResponse = OrderResponse.builder()
+                .orderStatus(order.getOrderStatus())
+                .orderDate(order.getOrderDate())
+                .amount(order.getAmount())
+                .orderId(order.getId())
+                .build();
+        return objectMapper.writeValueAsString(orderResponse);
+    }
+}
--- src/test/java/com/blagovestkabov/order_service/service/OrderServiceImplTest.java
+++ src/test/java/com/blagovestkabov/order_service/service/OrderServiceImplTest.java
@@ -5,23 +5,30 @@ import com.blagovestkabov.order_service.entity.Order;
 import com.blagovestkabov.order_service.exception.CustomException;
 import com.blagovestkabov.order_service.external.client.PaymentService;
 import com.blagovestkabov.order_service.external.client.ProductService;
+import com.blagovestkabov.order_service.external.request.PaymentRequest;
 import com.blagovestkabov.order_service.external.response.PaymentResponse;
 import com.blagovestkabov.order_service.external.response.ProductResponse;
+import com.blagovestkabov.order_service.model.OrderRequest;
 import com.blagovestkabov.order_service.model.OrderResponse;
 import com.blagovestkabov.order_service.model.PaymentMethod;
 import com.blagovestkabov.order_service.repository.OrderRepository;
+import org.h2.mvstore.type.LongDataType;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Instant;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 
 @SpringBootTest
 public class OrderServiceImplTest {
@@ -48,7 +55,7 @@ public class OrderServiceImplTest {
         // 4. Assert
         Order order = getMockedOrder();
 
-        Mockito.when(orderRepository.findById(ArgumentMatchers.anyLong())).thenReturn(Optional.of(order));
+        Mockito.when(orderRepository.findById(anyLong())).thenReturn(Optional.of(order));
 
         Mockito.when(restTemplate.getForObject("http://PRODUCT-SERVICE/product/" + order.getProductId(), ProductResponse.class)).thenReturn(getMockedProductResponse());
 
@@ -56,7 +63,7 @@ public class OrderServiceImplTest {
 
         OrderResponse orderResponse = orderService.getOrderDetails(1);
 
-        Mockito.verify(orderRepository, Mockito.times(1)).findById(ArgumentMatchers.anyLong());
+        Mockito.verify(orderRepository, Mockito.times(1)).findById(anyLong());
 
         assertNotNull(orderResponse);
 
@@ -66,13 +73,57 @@ public class OrderServiceImplTest {
     @Test
     void orderNotFound() {
 
-        Mockito.when(orderRepository.findById(ArgumentMatchers.anyLong())).thenReturn(Optional.ofNullable(null));
-        
+        Mockito.when(orderRepository.findById(anyLong())).thenReturn(Optional.ofNullable(null));
+
         CustomException customException = assertThrows(CustomException.class, () -> orderService.getOrderDetails(1));
 
         assertEquals("NOT_FOUND", customException.getErrorCode());
 
         assertEquals(404, customException.getStatusCode());
+    }
+
+    @Test
+    void successfulPlacedOrder() {
+        Order order = getMockedOrder();
+        OrderRequest orderRequest = getMockedOrderRequest();
+
+        Mockito.when(orderRepository.save(any(Order.class))).thenReturn(order);
+        Mockito.when(productService.reduceQuantity(anyLong(), anyLong())).thenReturn(new ResponseEntity<Void>(HttpStatus.OK));
+        Mockito.when(paymentService.doPayment(any())).thenReturn(new ResponseEntity<Long>(1L, HttpStatus.OK));
+
+        long orderId = orderService.placeOrder(orderRequest);
+
+        assertEquals(order.getId(), orderId);
+    }
+
+    @Test
+    void failedPlacedOrder() {
+        Order order = getMockedOrder();
+        OrderRequest orderRequest = getMockedOrderRequest();
+
+        Mockito.when(orderRepository.save(any(Order.class)))
+                .thenReturn(order);
+        Mockito.when(productService.reduceQuantity(anyLong(),anyLong()))
+                .thenReturn(new ResponseEntity<Void>(HttpStatus.OK));
+        Mockito.when(paymentService.doPayment(any(PaymentRequest.class)))
+                .thenThrow(new RuntimeException());
+
+        long orderId = orderService.placeOrder(orderRequest);
+        assertEquals(order.getId(), orderId);
+    }
+
+    @Test
+    public void placeOrderSuccessScenario() {
+
+    }
+
+    private OrderRequest getMockedOrderRequest() {
+        return OrderRequest.builder()
+                .paymentMethod(PaymentMethod.CASH)
+                .productId(1)
+                .quantity(10)
+                .totalAmount(100)
+                .build();
     }
 
     private PaymentResponse getMockedPaymentResponse() {
